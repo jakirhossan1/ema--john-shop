72150897b67f1451674cfb7d59de229a5a064ae9
jakirhossan1 <amargithub@gmail.com>
2021-10-03 14:04:51 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+
+          </Route>
+          <Route path="/orders">
+            <OrderReview></OrderReview>
+
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+
+          </Route>
+
+        </Switch>
+
+      </BrowserRouter>
+
     </div>
   );
 }
